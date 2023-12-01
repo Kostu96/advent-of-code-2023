6010a7cd8d40b5fc5cb5bfde48c75fe1062e0007
Konstanty <kostu96@gmail.com>
2023-12-01 17:44:34 +0100

    upload day 1 part 2

--- day 1/main.cpp
+++ day 1/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <array>
 
 unsigned int part1()
 {
@@ -23,8 +24,54 @@ unsigned int part1()
 
 unsigned int part2()
 {
+    std::array<const char*, 9> digitStrs{
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+    };
     unsigned int total = 0;
+    std::ifstream fin("input.txt");
+    std::string str{};
 
+    while (std::getline(fin, str))
+    {
+        unsigned int first_digit = 0, second_digit = 0;
+
+        auto pos1 = str.size();
+        for (auto i = 0; i < 9; i++)
+        {
+            auto new_pos = str.find(digitStrs[i]);
+            if (new_pos != str.npos && new_pos < pos1)
+            {
+                pos1 = new_pos;
+                first_digit = i + 1;
+            }
+        }
+
+        auto pos2 = str.find_first_of("123456789");
+        if (pos2 < pos1) {
+            first_digit = str[pos2] - '0';
+        }
+
+        pos1 = 0;
+        for (auto i = 0; i < 9; i++)
+        {
+            auto new_pos = str.rfind(digitStrs[i]);
+            if (new_pos != str.npos && new_pos > pos1)
+            {
+                pos1 = new_pos;
+                second_digit = i + 1;
+            }
+        }
+
+        pos2 = str.find_last_of("123456789");
+        if (pos2 >= pos1) {
+            second_digit = str[pos2] - '0';
+        }
+
+        unsigned int number = first_digit * 10 + second_digit;
+        total += number;
+    }
+
+    fin.close();
     return total;
 }
 
